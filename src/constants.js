ebce96edd90ddef29eefe96f536e1fa36049fcfe
Ajay <ajayrajnegi111@gmail.com>
2024-11-20 21:16:23 +0530

    All Files.

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of projects in that language. I also do app development. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/AjayRajNegi" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://my-resume-topaz-five.vercel.app/" target="_blank">Check it out?</a>. I haven't updated it in long time though😬
+  Contact me at ajayrajnegi111@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. `,
+  library: `I like reading philosophical books,`,
+  exit: `If you want to exit my portfolio, just close the tab.`,
+};
